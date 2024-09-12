6b2b10ad1a28b2aefe129c3d1a5a3b746eea5e77
Everton <evertoncefal1@gmail.com>
2024-09-11 22:27:25 -0300

    sistema de mensagem

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { select, input, checkbox } = require("@inquirer/prompts");
 
+let mensagem = "Bem vindo ao app de Metas";
+
 let meta = {
   value: "Tomar 3L de água por dia",
   checked: false,
@@ -10,7 +12,7 @@ let metas = [meta];
 const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta:" });
   if (meta.length == 0) {
-    console.log("A meta não pode ser vazia.");
+    mensagem = "A meta não pode ser vazia.";
     return;
   }
 
@@ -18,6 +20,7 @@ const cadastrarMeta = async () => {
     value: meta,
     checked: false,
   });
+  mensagem = "Meta cadastrada com sucesso";
 };
 
 listarMetas = async () => {
@@ -33,7 +36,7 @@ listarMetas = async () => {
   });
 
   if (respostas.length == 0) {
-    console.log("Nenhuma meta selecionada!");
+    mensagem = "Nenhuma meta selecionada!";
     return;
   }
 
@@ -44,7 +47,7 @@ listarMetas = async () => {
 
     meta.checked = true;
 
-    console.log("Meta(s) marcadas como concluída(s)");
+    mensagem = "Meta(s) marcada(s) como concluída(s)";
   });
 };
 
@@ -54,7 +57,7 @@ const metasRealizadas = async () => {
   });
 
   if (realizadas.length == 0) {
-    console.log("Não existem metas realizadas");
+    mensagem = "Não existem metas realizadas";
     return;
   }
 
@@ -70,7 +73,7 @@ const metasAbertas = async () => {
   });
 
   if (abertas.length == 0) {
-    console.log("Não existem metas abertas");
+    mensagem = "Não existem metas abertas";
     return;
   }
 
@@ -91,7 +94,7 @@ const deletarMetas = async () => {
   });
 
   if (itemsADeletar === 0) {
-    console.log("Nenhum item para deletar");
+    mensagem = "Nenhum item para deletar";
     return;
   }
 
@@ -100,12 +103,24 @@ const deletarMetas = async () => {
       return meta.value != item;
     });
 
-    console.log("Meta(s) deletada(s) com sucesoo");
+    mensagem = "Meta(s) deletada(s) com sucesso";
   });
+};
+
+const mostrarMensagem = () => {
+  console.clear();
+
+  if (mensagem != "") {
+    console.log(mensagem);
+    console.log("");
+    mensagem = "";
+  }
 };
 
 const start = async () => {
   while (true) {
+    mostrarMensagem();
+
     const opcao = await select({
       message: "Menu >",
       choices: [
@@ -139,7 +154,6 @@ const start = async () => {
     switch (opcao) {
       case "cadastrar":
         await cadastrarMeta();
-        console.log(metas);
         break;
       case "listar":
         await listarMetas();
