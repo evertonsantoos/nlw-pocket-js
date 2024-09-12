598fe22f79de5a02d8f47a839515c0d5db4187a2
Everton <evertoncefal1@gmail.com>
2024-09-11 21:52:47 -0300

    deletar metas

--- index.js
+++ index.js
@@ -80,6 +80,30 @@ const metasAbertas = async () => {
   });
 };
 
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => {
+    return { value: meta.value, checked: false };
+  });
+  const itemsADeletar = await checkbox({
+    message: "Selecione item para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  });
+
+  if (itemsADeletar === 0) {
+    console.log("Nenhum item para deletar");
+    return;
+  }
+
+  itemsADeletar.forEach((item) => {
+    metas.filter((meta) => {
+      return meta.value != item;
+    });
+
+    console.log("Meta(s) deletada(s) com sucesoo");
+  });
+};
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -102,6 +126,10 @@ const start = async () => {
           value: "abertas",
         },
         {
+          name: "Deletar metas",
+          value: "deletar",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -121,6 +149,9 @@ const start = async () => {
         break;
       case "abertas":
         await metasAbertas();
+        break;
+      case "deletar":
+        await deletarMetas();
         break;
       case "sair":
         console.log("Até a proóxima!");
